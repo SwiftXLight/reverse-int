e392d40860b6a5a46b112eb5decf62d404ec4abf
SwiftXLight <72575014+SwiftXLight@users.noreply.github.com>
2021-03-10 18:41:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-function reverse(x) {
-  if (x < 0) return -reverse(-x); // reverse(-123) === -reverse(123)
-  var str = x.toString(); // "123"
-  var strArray = str.split(""); // [ "1", "2", "3" ]
-  var revArray = strArray.reverse(); // [ "3", "2", "1" ]
-  var rev = revArray.join(""); // "321"
-  return Number(rev);
+function reverse(n) {  
+  return Number(Array.from(String(Math.abs(n))).reverse().join('')) * Math.sign(n);
 }
+
