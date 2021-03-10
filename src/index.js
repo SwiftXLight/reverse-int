406922fa5713fa8e6c8eb4966f8ba0fa3aa35b11
SwiftXLight <72575014+SwiftXLight@users.noreply.github.com>
2021-03-10 18:37:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-function reversedNum(num) {
-  return (
-    num
-      .toString()
-      .split('')
-      .reverse()
-      .join('')
-  ) * Math.sign(num)                 
+function reverse(x) {
+  if (x < 0) return -reverse(-x); // reverse(-123) === -reverse(123)
+  var str = x.toString(); // "123"
+  var strArray = str.split(""); // [ "1", "2", "3" ]
+  var revArray = strArray.reverse(); // [ "3", "2", "1" ]
+  var rev = revArray.join(""); // "321"
+  return Number(rev);
 }
